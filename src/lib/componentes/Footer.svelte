4f233fa5c22d19a8be568fab9e6fa1b4a4131801
Anderson de Paiva Sátiro <20221164010008@ifrn.local>
2025-06-27 11:42:17 -0300

    cria estrutura de rotas, componetes e do header global

---
+++ src/lib/componentes/Footer.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import { Instagram, Github, Twitter } from 'lucide-svelte';
+</script>
+
+<footer>
+    <section>
+        <h2>Assine nossa Newsletter</h2>
+        
+        <form>
+            <input>
+            <button>Clique aqui</button>
+        </form>
+    </section>
+
+    <div>
+        <div>
+            <nav>
+                <ul>
+                    <li>Sobre</li>
+                    <li>Sobre</li>
+                    <li>Sobre</li>
+                    <li>Sobre</li>
+                </ul>
+            </nav>
+
+            <div>
+                <Instagram size={32}/>
+                <Github size={32} />
+                <Twitter size={32} />
+            </div>
+        </div>
+
+        <hr />
+
+        <div>
+            <p>Biblioteca Web</p>
+        </div>
+    </div>
+</footer>
+
+<style>
+    footer {
+        display: flex;
+        background: rgb(255, 255, 150);
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script>
-    import Header from './Header.svelte'
-    import Main from './Main.svelte'
-    import Footer from './Footer.svelte'
+
+	import Main from "$lib/componentes/Main.svelte";
+
 </script>
 
-<Header parametro={101} />
-<Main />
-<Footer />
+
+<Main></Main>
+
