e3c28134ef48124b329b78af595e747e17296d64
Miguelm <mig355mendonca@gmail.com>
2025-07-11 11:50:37 -0300

    Iniciado o html do acervo

--- src/routes/acervo/+page.svelte
+++ src/routes/acervo/+page.svelte
@@ -1,1 +1,24 @@
-<h1>ACERVO</h1>
+<main>
+    <!-- menu scroll -->
+    <div>
+        <ul>
+            <li class="livro">
+                <a href="">
+                    <img class="livro-capa" src="" alt="">
+                    <p class="livro-titulo"></p>
+                    <p class="livro-autor"></p>
+                </a>
+            </li>
+            <li class="livro">
+                <a href="">
+                    <img class="livro-capa" src="" alt="">
+                    <p class="livro-titulo"></p>
+                    <p class="livro-autor"></p>
+                </a>
+            </li>
+        </ul>
+    </div>
+</main>
+<section>
+
+</section>
